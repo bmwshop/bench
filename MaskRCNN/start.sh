0340823d2f2978294bdaee0e490b3936a183bd04
Dima Rekesh <bmwshop@gmail.com>
2021-05-12 12:52:24 -0700

    removed start.sh

--- MaskRCNN/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# docker run --rm -v /home/nvidia/data:/data --gpus all --ipc=host -ti nvcr.io/nvidia/pytorch:21.04-py3 bash
-
-docker run --rm --gpus all --ipc=host -ti nvcr.io/nvidia/pytorch:21.04-py3 bash
