f28a8ca418c59fc4c04b4dce750dbcc9266deb05
2020chris <41304773+2020chris@users.noreply.github.com>
2021-06-17 18:38:36 -0700

    Update build.sh

--- BERT/build.sh
+++ BERT/build.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
-IMG="effnet_bench"
-CONTAINER_TF2x_BASE="nvcr.io/nvidia/tensorflow"
-CONTAINER_TF2x_TAG="21.05-tf2-py3"
 
-docker pull "${CONTAINER_TF2x_BASE}:${CONTAINER_TF2x_TAG}"
-
-echo -e "\n\nBuilding Effnet_test Container\n\n"
-
-sleep 1
-
-docker build -t ${IMG} \
-    --build-arg FROM_IMAGE_NAME="nvcr.io/nvidia/tensorflow:21.05-tf2-py3" \
-    .
+docker build -t foo .
